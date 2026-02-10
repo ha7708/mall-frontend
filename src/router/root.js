264c48f586ea4b785aecdba7b1c4bf9a850878c7
SeoYoung <ha7708@naver.com>
2026-02-10 17:23:52 +0900

    first commit - frontend

---
+++ src/router/root.js
@@ -0,0 +1,20 @@
+import { Suspense, lazy } from "react";
+// import { createBrowserRouter } from "react-router-dom";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+const Loading = <div>Loading...</div>;
+const Main = lazy(() => import("../pages/MainPage"));
+
+const root = createBrowserRouter([
+  {
+    path: "",
+    element: (
+      <Suspense fallback={Loading}>
+        <Main />
+      </Suspense>
+    ),
+  },
+]);
+
+export default root;
