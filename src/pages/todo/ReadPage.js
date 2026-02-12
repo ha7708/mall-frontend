1c0b9ab213ca249ab18da8e376e32ec876445b65
SY Jung <104877247+ha7708@users.noreply.github.com>
2026-02-12 16:48:26 +0900

    커스텀 훅 적용후

--- src/pages/todo/ReadPage.js
+++ src/pages/todo/ReadPage.js
@@ -1,52 +1,12 @@
-import {
-  createSearchParams,
-  useNavigate,
-  useParams,
-  useSearchParams,
-} from "react-router-dom";
-import { useCallback } from "react";
+import { useParams } from "react-router-dom";
+
 import ReadComponent from "../../components/todo/ReadComponent";
 
 // 조회할 때 사용하는 ReadPage 컴포넌트
 const ReadPage = () => {
   const { tno } = useParams();
-  const navigate = useNavigate();
-  // 쿼리스트링을 유지하여 navigate() 이동시에 활용하기
-  // 1. useSearchParams() : 쿼리스트링으로 전달된 데이터를 확인하고
-  const [queryParams] = useSearchParams();
-
-  // 2. 삼항식을 사용해서 변수로 추출
-  const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1;
-  const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10;
-
-  // 3. 추출한 변수와 createSearchParams() 함수로 navigate 이동 시 필요한 쿼리스트링 만듬
-  const queryStr = createSearchParams({ page, size }).toString();
-
-  // 수정 : tno 값으로 특정 페이지 이동
-  const moveToModify = useCallback(
-    (tno) => {
-      navigate({
-        pathname: `/todo/modify/${tno}`,
-        // 4. navigate의 search 파라미터에 3에서 만든 쿼리스트링 실어서 보냄
-        search: queryStr,
-      });
-    },
-    [tno, page, size]
-  );
-
-  // 조회
-  const moveToList = useCallback(() => {
-    navigate({ pathname: `/todo/list`, search: queryStr });
-  }, [page, size]);
 
   return (
-    // <div className="text-3xl font-extrabold">
-    //   Todo Read Page Component {tno}
-    //   <div>
-    //     <button onClick={() => moveToModify(33)}>Test Modify</button>
-    //     <button onClick={() => moveToList()}>Test List</button>
-    //   </div>
-    // </div>
     <div className="font-extrabold w-full bg-white mt-6">
       <div className="text-2xl ">Todo Read Page Component {tno}</div>
       <ReadComponent tno={tno} />
